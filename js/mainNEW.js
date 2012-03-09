f1dc3f3bd58ee24df3e525714efa9146bb277dd3
Nick Weil <speedquest@fullsail.edu>
2012-03-09 00:18:02 -0500

    Reverted back to an older version where Edit/Delete worked, then re-added the Data Import code.

--- js/mainNEW.js
+++ js/mainNEW.js
@@ -2,85 +2,92 @@
 Nick Weil
 ASD
 March 2012
-Project 2
-March 8,2012
+Project 1
+March 2,2012
 
- */
+*/
 
-	// Save Function
-	$('#submit').live('click', function saveData(id) {
-		var d = new Date();
-		var key = (d.getTime());
-		var year = $("#year").val();
-		var manufacturer = $("#manufacturer").val();
-		var model = $("#model").val();
-		var lastOilDate = $("#lastOilDate").val();
-		var synthetic;
-		if ($('#synthetic').is(":checked")){
-			synthetic = "Yes"
-		}else{
-			synthetic = "No"
-		}
-		var oilDuration = $("#oilDuration").val();
-		var notes = $("#notes").val();
-		var item = [
-		            year, manufacturer, model, lastOilDate, synthetic, oilDuration, notes];
-		localStorage.setItem(key, item);
-		location.reload();
-		alert("Vehicle Saved!");
-	});	
 
-var toggleControls = function (n) {
+
+
+    // Save Function
+    $('#submit').live('click', function saveData(id) {
+	var d = new Date();
+	var key = (d.getTime());
+	var year = $("#year").val();
+	var manufacturer = $("#manufacturer").val();
+	var model = $("#model").val();
+	var lastOilDate = $("#lastOilDate").val();
+	var synthetic;
+	if ($('#synthetic').is(":checked")){
+	    synthetic = "Yes"
+	    }else{
+	    synthetic = "No"
+	    }
+	var oilDuration = $("#oilDuration").val();
+	var notes = $("#notes").val();
+	var item = [
+	year, manufacturer, model, lastOilDate, synthetic, oilDuration, notes];
+    
+	localStorage.setItem(key, item);
+	location.reload();
+	alert("Vehicle Saved!");
+	console.log(localStorage);
+    });	
+    
+    var toggleControls = function (n) {
 	switch (n) {
 	case "on":
-		$('#addPage').css('display', 'none');
-		break;
+	    $('#addItem').css('display', 'none');
+	    break;
 	case "off":
-		$('#addPage').css('display', 'block');
-		$('#list').css('display', 'none');
-		break;
+	    $('#addItem').css('display', 'block');
+	    $('#list').css('display', 'none');
+	    break;
 	default:
-		return false;
+	    return false;
 	}
-}
-
-
-//Get Data Function
-$('#displayLink').live('click', function getData() {
-	toggleControls("on");
+    }
+    
+    $('#displayLink').live('click', function getData() {
+	    toggleControls("on");
 	var getListdiv = $('#list')[0];
 	for (var i = 0, j = localStorage.length; i < j; i++) {
-		var key = localStorage.key(i);
-		var value = localStorage.getItem(key);
-		value = value.split(',');
-		$('<div>').attr({
-			'class': 'listDiv'
-		}).appendTo('#list');
-		$('<h3>').html(value[1]).appendTo('.listDiv');
-		$('<p>').html('Year: ' + value[0]).appendTo('.listDiv');
-		$('<p>').html('Model: ' + value[2]).appendTo('.listDiv');
-		$('<p>').html('Last Oil Change: ' + value[3]).appendTo('.listDiv');
-		$('<p>').html('Duration: ' + value[4]).appendTo('.listDiv');
-		$('<p>').html('Synthetic: ' + value[5]).appendTo('.listDiv');
-		$('<p>').html('Notes: ' + value[6]).appendTo('.listDiv');
-		$('<p>').html($('<a>').attr({
-			'href': '#',
-			'onclick': 'deleteItem(' + key + ');'
-		}).html('Delete Vehicle')).appendTo('.listDiv');
-		$('<p>').html($('<a>').attr({
-			'href': '#',
-			'onclick': 'editItem(' + key + ');'
-		}).html('Edit Vehicle')).appendTo('.listDiv');
-
+	    var key = localStorage.key(i);
+	    var value = localStorage.getItem(key);
+	    value = value.split(',');
+    
+	    $('<div>').attr({
+		'class': 'listDiv'
+	    }).appendTo('#list');
+	    $('<h3>').html(value[1]).appendTo('.listDiv');
+	    $('<p>').html('Year: ' + value[0]).appendTo('.listDiv');
+	    $('<p>').html('Model: ' + value[2]).appendTo('.listDiv');
+	    $('<p>').html('Last Oil Change: ' + value[3]).appendTo('.listDiv');
+	    $('<p>').html('Duration: ' + value[4]).appendTo('.listDiv');
+	    $('<p>').html('Synthetic: ' + value[5]).appendTo('.listDiv');
+	    $('<p>').html('Notes: ' + value[6]).appendTo('.listDiv');
+	    $('<p>').html($('<a>').attr({
+		'href': '#',
+		'onclick': 'deleteItem(' + key + ');'
+	    }).html('Delete Vehicle')).appendTo('.listDiv');
+	    $('<p>').html($('<a>').attr({
+		'href': '#',
+		'onclick': 'editItem(' + key + ');'
+	    }).html('Edit Vehicle')).appendTo('.listDiv');
+    
 	}
-});
-
-//EDIT MY DATA!!
-var editItem = function (id) {
+    });
+    
+    
+    
+    // EDIT MY DATA!!
+    
+    var editItem = function (id) {
 	var itemId = id;
-	var value = localStorage.getItem(itemId);
-	value = value.split(',');
-	toggleControls("off");
+	    var value = localStorage.getItem(itemId);
+	    value = value.split(',');
+	    toggleControls("off");
 	var year = value[0];
 	var manufacturer = value[1];
 	var model = value[2];
@@ -88,80 +95,72 @@ var editItem = function (id) {
 	var synthetic;
 	var oilDuration = value[5];
 	var notes = value[6];
-
+    
 	$('#year').val(year);
 	$('#manufacturer').val(manufacturer);
 	$('#model').val(model);
 	$('#lastOilDate').val(lastOilDate);
 	if ($('#synthetic').is(":checked")){
-		synthetic = "Yes"
-	}else{
-		synthetic = "No"
-	}
-	$('#oilDuration').val(oilDuration);
+	    synthetic = "Yes"
+	    }else{
+	    synthetic = "No"
+	    }
+	    $('#oilDuration').val(oilDuration);
 	$('#notes').val(notes);
-
-	// Show edit item button, hide submit button
+    
+	// show edit item button, hide submit button
 	var editButton = $('#edit-item-button').css('display', 'block');
 	var subresButtons = $('#submit-reset-buttons').css('display', 'none');
 	var itemList = $('#list').css('display', 'none');
-
-	// What happens when clicking editItem button
-	$('#edit-item').live('click', function clickEdit() {
-		var year = $('#year').val();
-		var manufacturer = $('#manufacturer').val();
-		var model = $('#model').val();
-		var lastOilDate = $('#lastOilDate').val();
-		var synthetic;
-		if ($('#synthetic').is(":checked")){
-			synthetic = "Yes"
-		}else{
-			synthetic = "No"
-		}
-		var oilDuration = $('#oilDuration').val();
-		var notes = $('#notes').val();
-		var item = [
-		            year,
-		            manufacturer,
-		            model,
-		            lastOilDate,
-		            synthetic,
-		            oilDuration,
-		            notes];
-		localStorage.setItem(itemId, item);           
-		location.reload();
-		alert("Vehicle Edited!");
-
+    
+	// when clicking editItem button
+	$('#editItem').live('click', function clickEdit() {
+	    var year = $('#year').val();
+	    var manufacturer = $('#manufacturer').val();
+	    var model = $('#model').val();
+	    var lastOilDate = $('#lastOilDate').val();
+	    var synthetic;
+	    if ($('#synthetic').is(":checked")){
+		    synthetic = "Yes"
+		    }else{
+		    synthetic = "No"
+		    }
+	    var oilDuration = $('#oilDuration').val();
+	    var notes = $('#notes').val();
+	    var item = [
+	    year, manufacturer, model, lastOilDate, synthetic, oilDuration, notes];
+	 
+	    localStorage.setItem(itemId, item);           
+	    location.reload();
+	    alert("Vehicle Edited!");
+	    
 	});
-
-}
-
-
-//DELETE AN ITEM
-
-var deleteItem = function (id) {
+    
+    }
+    
+    
+    // DELETE AN ITEM
+    
+    var deleteItem = function (id) {
 	var ask = confirm("Are you sure you want to delete this Vehicle?");
 	if (ask) {
-		localStorage.removeItem(id);
-		window.location.reload();
+	    localStorage.removeItem(id);
+	    window.location.reload();
 	} else {
-		alert("Your vehicle was not removed.");
+	    alert("Your vehicle was not removed.");
 	}
-}
+    }
 
-//CLEAR LOCAL STORAGE 
 function clearLocal() {
-	if (localStorage.length === 0) {
-		alert("There is no data to clear.");
-	} else {
-		localStorage.clear();
-		alert("All vehicles have been deleted");
-		window.location.reload();
-		return false;
-	}
+   if (localStorage.length === 0) {
+       alert("There is no data to clear.");
+   } else {
+       localStorage.clear();
+       alert("All vehicles have been deleted");
+       window.location.reload();
+       return false;
+   }
 }
-
-// EACH of the (3) data types: JSON, XML, CSV
 
 //JSON Data 
 $('#jsonbutton').bind('click', function(){
